6bbc5f4f822afdd0c7786890cad04ae72b5d5dd7
Eliezer <eliezer1789j@gmail.com>
2020-02-07 12:59:01 +0000

    criando o feature module de pessoas

---
+++ src/app/pessoas/pessoas.module.ts
@@ -0,0 +1,38 @@
+import { FormsModule } from '@angular/forms';
+import { InputMaskModule } from 'primeng/inputmask';
+import { TooltipModule } from 'primeng/tooltip';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+
+
+
+@NgModule({
+  declarations: [
+    PessoasPesquisaComponent,
+    PessoaCadastroComponent,
+    PessoasGridComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputMaskModule
+  ],
+  exports: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent
+  ]
+})
+export class PessoasModule { }
