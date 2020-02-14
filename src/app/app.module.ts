a7d36a994996770071db35b29e2705a330c5270f
Eliezer <eliezer1789j@gmail.com>
2020-02-14 01:18:03 +0000

    Configurando rotas na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
@@ -12,6 +16,12 @@ import { LancamentosModule } from './lancamentos/lancamentos.module';
 
 registerLocaleData(localePt);
 
+const routes: Routes = [
+  { path: 'lancamentos', component: LancamentosPesquisaComponent },
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
+  { path: 'pessoas', component: PessoasPesquisaComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +31,7 @@ registerLocaleData(localePt);
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
+    RouterModule.forRoot(routes),
 
     LancamentosModule,
     PessoasModule
