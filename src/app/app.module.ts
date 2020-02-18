66b5893bb9eaec4dd598e27bc42b11444b77c565
Eliezer <eliezer1789j@gmail.com>
2020-02-18 21:42:22 +0000

    Refatorando as rotas para usar Routing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,18 @@
-import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { registerLocaleData } from '@angular/common';
-import localePt from '@angular/common/locales/pt';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { AppRoutingModule } from './app-routing.module';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 registerLocaleData(localePt);
-
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' }, // redireciona para outro path
-  { path: 'lancamentos', component: LancamentosPesquisaComponent },
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
-  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
-  { path: 'pessoas', component: PessoasPesquisaComponent },
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
-  // wildcard '**': qualquer coisa que não tiver uma rota configurada
-  { path: '**', redirectTo: 'pagina-nao-encontrada' }
-];
 
 @NgModule({
   declarations: [
@@ -36,14 +22,13 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    CoreModule,
-    RouterModule.forRoot(routes),
 
+    CoreModule,
     LancamentosModule,
-    PessoasModule
+    PessoasModule,
+    AppRoutingModule
   ],
-  providers: [
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
