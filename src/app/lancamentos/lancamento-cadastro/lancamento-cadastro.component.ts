436f90c41b4b7d34c8eea32352d79ddf15687f8d
Eliezer <eliezer1789j@gmail.com>
2020-02-18 15:33:08 +0000

    Preenchendo os campos na edição de lançamento

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -44,8 +44,27 @@ export class LancamentoCadastroComponent implements OnInit {
     // tslint:disable-next-line: no-string-literal
     console.log(this.route.snapshot.params['codigo']);
 
+    // tslint:disable-next-line: no-string-literal
+    const codigoLancamento = this.route.snapshot.params['codigo'];
+
+    if (codigoLancamento) {
+      this.carregarLancamento(codigoLancamento);
+    }
+
     this.carregarCategorias();
     this.carregarPessoas();
+  }
+
+  get editando() {
+    return Boolean(this.lancamento.codigo);
+  }
+
+  carregarLancamento(codigo: number) {
+    this.lancamentoService.buscarPorCodigo(codigo)
+      .then(lancamento => {
+        this.lancamento = lancamento;
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 
   // método que chama o serviço para salvar um lançamento
