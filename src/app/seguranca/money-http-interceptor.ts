81de3d35847b8a08255becbd05b1f69aaa1ccdb1
Eliezer <eliezer1789j@gmail.com>
2020-02-28 14:07:24 +0000

    E se o Refresh Token expirar? redireciona para pagina de login

--- src/app/seguranca/money-http-interceptor.ts
+++ src/app/seguranca/money-http-interceptor.ts
@@ -4,7 +4,7 @@ import { mergeMap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 
-
+export class NotAuthenticatedError {}
 
 @Injectable()
 export class MoneyHttpInterceptor implements HttpInterceptor {
@@ -16,12 +16,15 @@ export class MoneyHttpInterceptor implements HttpInterceptor {
           return from(this.auth.obterNovoAccessToken())
               .pipe(
                   mergeMap(() => {
-                      req = req.clone({
+                    if (this.auth.isAccessTokenInvalido()) {
+                      throw new NotAuthenticatedError();
+                    }
+                    req = req.clone({
                           setHeaders: {
                               Authorization: `Bearer ${localStorage.getItem('token')}`
                           }
                       });
-                      return next.handle(req);
+                    return next.handle(req);
                   })
               );
           }
