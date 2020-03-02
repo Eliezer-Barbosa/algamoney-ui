db13ae52d6751de23652b0e0aab56ca470089b98
Eliezer <eliezer1789j@gmail.com>
2020-03-02 02:38:31 +0000

    passing white and blacklists to environments files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://eliezer-java-api.herokuapp.com'
+  apiUrl: 'https://eliezer-java-api.herokuapp.com',
+  whiteListedDomains: ['eliezer-java-api.herokuapp.com'],
+  blackListedRoutes: ['https://eliezer-java-api.herokuapp.com/oauth/token']
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080',
+  whiteListedDomains: ['localhost:8080'],
+  blackListedRoutes: ['http://localhost:8080/oauth/token']
 };
