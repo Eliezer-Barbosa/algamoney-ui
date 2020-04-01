3cb2ce319238d86a0913c4a68d0851dd93ac69bd
Eliezer <eliezer1789j@gmail.com>
2020-04-01 20:33:46 +0100

    Configurando formulário do relatório

--- src/app/relatorios/relatorio-lancamentos/relatorio-lancamento.component.ts
+++ src/app/relatorios/relatorio-lancamentos/relatorio-lancamento.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RelatorioLancamentoComponent implements OnInit {
 
+  periodoInicio: Date;
+  periodoFim: Date;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  gerar() {
+    console.log('Inicio: ' + this.periodoInicio);
+    console.log(`Fim: ${this.periodoFim}`);
   }
 
 }
