723eebdcd0c96f49e818480316c55f77cd4c63e6
Eliezer <eliezer1789j@gmail.com>
2020-04-05 18:09:05 +0100

    Atualizando o módulo JWT

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://eliezer-java-api.herokuapp.com'
+  apiUrl: 'https://eliezer-java-api.herokuapp.com',
+
+  tokenWhitelistedDomains: [ /eliezer-java-api.herokuapp.com/ ],
+  tokenBlacklistedRoutes: [/\/oauth\/token/]
 };
